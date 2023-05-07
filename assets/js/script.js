e3edb1334bd0d3a16f49db1858bef0217b06bab1
Shane <sb_94@yahoo.com>
2023-05-06 17:01:37 -0700

    pseudocoded what I will be needing in my js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
+// Create a variable to hold API key
 var apiKey = "0874d781cac886c8251059cc7a09d09a";
+
+// Create variables needed
+var searchForm = document.querySelector("form");
+var searchInput = document.querySelector("input");
+var cityDateEl = document.getElementById("city-date");
+var currentWeatherEl = document.getElementById("current-weather-data");
+var forecastEl = document.querySelector(".forecast-cards");
+
+// Get the city name from input field
+// Use OpenWeather API to get current weather data for that city
+// Use OpenWeather API to get the 5-day forecast data for that city
+
+// Display current weather data and 5-day forecast on the page
+
+// Store the city name in local storage when user generates a search
+
+// Display the list of city names in the search history section
+
+// When user clicks on a city from search history, retrieve data to be displayed again on page
